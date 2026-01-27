e95d272e24c8684cd27f8afc75875a03f8026f1f
Simon Leyland <simonleyland1977@gmail.com>
2026-01-27 22:27:43 +0000

    chore: temporarily disable sentry to debug build

--- next.config.ts
+++ next.config.ts
@@ -5,26 +5,25 @@ const nextConfig: NextConfig = {
   // output: "export", // Disabled to allow SSR on Amplify
 };
 
-export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-
-  org: "fiftysix-ai",
-  project: "javascript-nextjs",
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-
-
-  // Upload source maps to Sentry
-  sourcemaps: {
-    disable: false,
-  },
-});
+export default nextConfig;
+// export default withSentryConfig(nextConfig, {
+//   // For all available options, see:
+//   // https://github.com/getsentry/sentry-webpack-plugin#options
+// 
+//   org: "fiftysix-ai",
+//   project: "javascript-nextjs",
+// 
+//   // Only print logs for uploading source maps in CI
+//   silent: !process.env.CI,
+// 
+//   // For all available options, see:
+//   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+// 
+//   // Upload a larger set of source maps for prettier stack traces (increases build time)
+//   widenClientFileUpload: true,
+// 
+//   // Upload source maps to Sentry
+//   sourcemaps: {
+//     disable: false,
+//   },
+// });
